d1c339358c114105e0020401fd5840869967a062
Raghav67816 <kumaraghav079@gmail.com>
2026-07-19 17:51:37 +0530

    initial commit

---
+++ font_converter/build/CMakeFiles/nanofont.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/nanofont.dir/link.d"
+  "CMakeFiles/nanofont.dir/main.cpp.o"
+  "CMakeFiles/nanofont.dir/main.cpp.o.d"
+  "nanofont"
+  "nanofont.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/nanofont.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
